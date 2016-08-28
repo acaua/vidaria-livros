8bd855196207f9239d85d6564885a282f710cdcf
Acaua Sperl de Faria <acaua.faria@gmail.com>
2016-08-28 20:10:22 -0300

    Change navbar collapse to Bootstrap Affix instead of .scroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 $(document).ready(function() {
+  // Initialize affix and add an offset to add affix class on scroll
+  $('#main-nav').affix({
+    offset: {
+      top: 500
+    }
+  });
+
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function(){
           $('.navbar-toggle:visible').click();
