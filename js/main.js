9c402976e101900d4879bffafb9e30648fe9de40
Acaua Sperl de Faria <acaua.faria@gmail.com>
2016-09-27 14:34:37 -0300

    Change CSS to SCSS (add SCSS file and CSS generated file), add visual changes, change some images

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
   });
 
   // Closes the Responsive Menu on Menu Item Click
-  $('.navbar-collapse ul li a').click(function(){
-          $('.navbar-toggle:visible').click();
+  $('.navbar-collapse ul li a, .navbar-brand.page-scroll').click(function(){
+    $('.navbar-ex1-collapse').collapse('hide');
   });
 
   //jQuery for page scrolling feature - requires jQuery Easing plugin
   $('a.page-scroll').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
   });
 
   // Assigning Ajax post of contact form instead of normal submit behavior
@@ -28,22 +28,22 @@ $(document).ready(function() {
     var url = base_url + atob(base64_email);
     var message = $("#contact-form").serialize();
     $.ajax({
-        url: url,
-        method: "POST",
-        data: message,
-        dataType: "json",
-        beforeSend: function() {
-          $("#form-submit-button").prop("disabled",true).html("Enviando...");
-        },
-        success:function(data) {
-            $("#form-block").hide();
-            $("#form-error").hide();
-            $("#form-success").show();
-        },
-        error: function(err) {
-          $("#form-submit-button").prop("disabled",false).html("Enviar");
-          $("#form-error").show();
-        }
+      url: url,
+      method: "POST",
+      data: message,
+      dataType: "json",
+      beforeSend: function() {
+        $("#form-submit-button").prop("disabled",true).html("Enviando...");
+      },
+      success:function(data) {
+        $("#form-block").hide();
+        $("#form-error").hide();
+        $("#form-success").show();
+      },
+      error: function(err) {
+        $("#form-submit-button").prop("disabled",false).html("Enviar");
+        $("#form-error").show();
+      }
     });
   });
 
